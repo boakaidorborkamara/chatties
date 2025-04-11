fe2437d01d03540f2ed239f109bb28113851c022
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2025-04-11 18:53:40 +0000

    style sender's message

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -36,12 +36,12 @@ export function ChatArea() {
       </div>
 
       {/* chat content  */}
-      <div className="px-10 my-10 ">
+      <div className="px-10  ">
         {/* chat for person with the account  */}
-        <div className="flex  gap-3 w-1/2">
+        <div className="flex my-10  gap-3 w-7/12">
           {/* profile image  */}
           <div>
-            <img src="/assets/Ellipse1.png" alt="" className="w-50" />
+            <img src="/assets/Ellipse1.png" alt="" className="w-20" />
           </div>
 
           {/* username and message  */}
@@ -53,14 +53,33 @@ export function ChatArea() {
             <p className="text-slate-400 bg-white  p-5 mt-2 rounded-b-lg rounded-tr-lg shadow-md">
               I a coming at your house. what do you want us to do for you this
               morning? I a coming at your house. what do you want us to do for
-              you this morning? I a coming at your house. what do you want us to
-              do for you this morning?I a coming at your house. what do you want
-              us to do for you this morning?
+              you this morning?
             </p>
           </div>
         </div>
 
         {/* chat for person communicating with account owner  */}
+        <div className="flex justify-end p-0 ">
+          <div className="flex justify-end  my-10  gap-3 w-7/12">
+            {/* username and message  */}
+            <div>
+              <div className="flex gap-10 items-center justify-end ">
+                <small className="text-slate-400">10:30 AM</small>
+                <h4 className="font-bold">Boakai Kamara</h4>
+              </div>
+              <p className="text-white bg-blue-400  p-5 mt-2 rounded-b-lg rounded-tr-lg shadow-md">
+                I a coming at your house. what do you want us to do for you this
+                morning? I a coming at your house. what do you want us to do for
+                you this morning?
+              </p>
+            </div>
+
+            {/* profile image  */}
+            <div>
+              <img src="/assets/Ellipse1.png" alt="" className="w-20" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
