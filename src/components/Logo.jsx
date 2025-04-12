96e21ddffa134666d6bf2c13ca587928d0954142
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2025-04-12 17:00:15 +0000

    style signup page

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="p-5 gap-1 flex">
+      <div className="flex items-center">
+        <img src="/assets/mingcute_chat-4-fill.png" alt="Chatties logo" />
+        <img src="/assets/Chatties.png" alt="Chatties logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
