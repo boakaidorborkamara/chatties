017e9f006b5ee0db35751d5a25689b4e352c7edb
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2025-04-12 17:03:10 +0000

    style login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <section className="bg-blue-400 h-lvh flex justify-center items-center">
+      {/* signup form container  */}
+      <div className="bg-white p-10 w-2/6 rounded-2xl">
+        {/* logo  */}
+        <div className="pb-3 gap-1 justify-center items-center flex">
+          <div className="flex items-center">
+            <img src="/assets/mingcute_chat-4-fill.png" alt="Chatties logo" />
+            <img src="/assets/Chatties.png" alt="Chatties logo" />
+          </div>
+        </div>
+
+        <h2 className="text-center text-3xl font-bold mb-8">Login</h2>
+
+        <form action="" className="mb-7">
+          <div className="flex flex-col mb-5">
+            <label htmlFor="username" className="mb-2">
+              Username
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="appearance-none border rounded p-3 "
+            />
+          </div>
+
+          <div className="flex flex-col mb-5">
+            <label htmlFor="password" className="mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="p-3 appearance-none border rounded "
+            />
+          </div>
+        </form>
+
+        <p>
+          Already have an account?{" "}
+          <a href="/signup" className="text-blue-400 font-medium">
+            Signup
+          </a>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Login;
